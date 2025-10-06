6101331293dd37bb7041b752a42fb8c95a519977
anuj05 <anujvishwakarma33@gmail.com>
2025-10-07 01:53:18 +0530

    auth_update

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # Happy Coding!
 # 2025
-
-
-# hey, checkout my this all @app file and also gothrough this "https://nextjs.org/learn/dashboard-app/adding-authentication" site and fix the all error in file   
--- auth.ts
+++ auth.ts
@@ -6,11 +6,14 @@ import Credentials from 'next-auth/providers/credentials';
 import { z } from 'zod';
 import type { User } from '@/app/lib/definitions';
 import bcrypt from 'bcrypt';
+import postgres from 'postgres';
 
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+ 
 async function getUser(email: string): Promise<User | undefined> {
   try {
-    const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
-    return user.rows[0];
+    const user = await sql<User[]>`SELECT * FROM users WHERE email=${email}`;
+    return user[0];
   } catch (error) {
     console.error('Failed to fetch user:', error);
     throw new Error('Failed to fetch user.');
@@ -28,6 +31,7 @@ export const { auth, signIn, signOut } = NextAuth({
 
         if (parsedCredentials.success) {
           const { email, password } = parsedCredentials.data;
+
           const user = await getUser(email);
           if (!user) return null;
           const passwordsMatch = await bcrypt.compare(password, user.password);
